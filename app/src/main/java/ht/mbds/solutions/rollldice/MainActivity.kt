188f5783878c97fb159b27f1de60024219bf758b
Jeffking Sterile <jsterile@solutions.ht>
2024-07-28 20:56:15 -0400

    Etape 2 Transformer l'application en Casino

--- app/src/main/java/ht/mbds/solutions/rollldice/MainActivity.kt
+++ app/src/main/java/ht/mbds/solutions/rollldice/MainActivity.kt
@@ -1,5 +1,16 @@
 package ht.mbds.solutions.rollldice
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -13,7 +24,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnRoll.setOnClickListener{ rollDice(binding.diceNumber1Txt) }
+        binding.btnRoll.setOnClickListener{
+            rollDice(binding.diceNumber1Txt)
+            rollDice(binding.diceNumber1Txt2)
+            if (binding.diceNumber1Txt.text.trim() == binding.diceNumber1Txt2.text.trim()){
+                Toast.makeText(this,"Vous avez gagné, Félicitation",Toast.LENGTH_LONG).show()
+                MediaPlayer.create(this, R.raw.plucky).start();
+            }
+        }
     }
 }
 
@@ -22,3 +40,4 @@ private fun rollDice(diceTxtView : TextView ) {
     val diceRoll = dice.roll()
     diceTxtView.text = diceRoll.toString()
 }
+
