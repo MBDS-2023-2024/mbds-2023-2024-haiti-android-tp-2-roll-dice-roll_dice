138534c81f62f95169eb2c28e03ddae8b8ef5d9f
Jeffking Sterile <jsterile@solutions.ht>
2024-07-28 20:03:54 -0400

    Etape 1

--- app/src/main/java/ht/mbds/solutions/rollldice/MainActivity.kt
+++ app/src/main/java/ht/mbds/solutions/rollldice/MainActivity.kt
@@ -2,7 +2,9 @@ package ht.mbds.solutions.rollldice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
+import ht.mbds.solutions.rollldice.classes.Dice
 import ht.mbds.solutions.rollldice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnRoll.setOnClickListener{
-            binding.diceNumber1.text = "2"
-            Toast.makeText(this,"Dice have been rolled",Toast.LENGTH_LONG).show()
-        }
+        binding.btnRoll.setOnClickListener{ rollDice(binding.diceNumber1Txt) }
     }
+}
+
+private fun rollDice(diceTxtView : TextView ) {
+    val dice = Dice(6)
+    val diceRoll = dice.roll()
+    diceTxtView.text = diceRoll.toString()
 }
