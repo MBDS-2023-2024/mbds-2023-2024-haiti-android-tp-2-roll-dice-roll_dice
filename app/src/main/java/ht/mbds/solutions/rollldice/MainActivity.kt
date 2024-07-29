f651c0af297a197cfad6a208c585a21b98c3f9e1
Jeffking Sterile <jsterile@solutions.ht>
2024-07-28 23:19:32 -0400

    Etape 2 Faites vos jeux

--- app/src/main/java/ht/mbds/solutions/rollldice/MainActivity.kt
+++ app/src/main/java/ht/mbds/solutions/rollldice/MainActivity.kt
@@ -13,6 +13,10 @@ import android.net.Uri
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import ht.mbds.solutions.rollldice.classes.Dice
@@ -24,20 +28,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        var thisActivity : Context = this
+        validation(thisActivity)
         binding.btnRoll.setOnClickListener{
-            rollDice(binding.diceNumber1Txt)
-            rollDice(binding.diceNumber1Txt2)
-            if (binding.diceNumber1Txt.text.trim() == binding.diceNumber1Txt2.text.trim()){
-                Toast.makeText(this,"Vous avez gagné, Félicitation",Toast.LENGTH_LONG).show()
-                MediaPlayer.create(this, R.raw.plucky).start();
-            }
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val dice1 = rollDice(binding.diceNumber1Txt)
+        val dice2 = rollDice(binding.diceNumber1Txt2)
+        val total = dice1 + dice2
+        /*if (binding.diceNumber1Txt.text.trim() == binding.diceNumber1Txt2.text.trim()){
+            Toast.makeText(this,"Vous avez gagné, Félicitation",Toast.LENGTH_LONG).show()
+            MediaPlayer.create(this, R.raw.plucky).start();
+        }*/
+        if (total== binding.entryNumberTxt.text.trim().toString().toInt()){
+            Toast.makeText(this,"Vous avez gagné, Félicitation",Toast.LENGTH_LONG).show()
+            MediaPlayer.create(this, R.raw.plucky).start();
+        }
+    }
+    private fun validation(thisActivity: Context) {
+        binding.entryNumberTxt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val enteredText: String = s.toString()
+                if (enteredText.isNullOrEmpty()){
+                    binding.btnRoll.isEnabled = false
+                    Toast.makeText(thisActivity,"Veuillez entre un nombre entre 1 à 18",Toast.LENGTH_LONG).show()
+                }else{
+                    if (enteredText.toLong() <1 || enteredText.toLong() >18){
+                        binding.btnRoll.isEnabled = false
+                        Toast.makeText(thisActivity,"nombre doit etre superieur à 0 et inferieur à 19",Toast.LENGTH_LONG).show()
+                    }else{
+                        binding.btnRoll.isEnabled = true
+                    }
+                }
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
     }
 }
 
-private fun rollDice(diceTxtView : TextView ) {
+private fun rollDice(diceTxtView : TextView ) : Int {
     val dice = Dice(6)
     val diceRoll = dice.roll()
     diceTxtView.text = diceRoll.toString()
+    return diceRoll
 }
+
+
 
